375f2054471d28b5f54b766fd095b9101f67e041
zodiacfly <renzodiac@hotmail.com>
2019-01-29 08:47:28 +1100

    checkbox

---
+++ checkbox/js/common.js
@@ -0,0 +1,44 @@
+
+function addLoadEvent(func){
+	var oldLoad=window.onload;
+	if (window.onload != 'function'){
+		window.onload = func;
+	} else {
+		window.onload = function(){
+			oldLoad();
+			func();
+		}
+	}
+}
+
+function checkboxSelectAll(){
+
+	var selectAll = document.getElementById('all');
+	var inner = document.getElementById('inner');
+	var list = inner.getElementsByTagName('input');
+
+	function checkboxList(){
+		var flag = true;
+		for (var i = 0; i < list.length; i++) {
+			if(!list[i].checked){
+				flag = false;
+				break;
+			}
+		}
+		selectAll.checked = flag;
+	}
+
+	for (var i = 0; i < list.length; i++) {
+		list[i].onclick = checkboxList;
+	}
+
+	selectAll.onclick = function(){
+		for (var i = 0; i < list.length; i++) {
+			list[i].checked = this.checked;
+		}
+	};
+	
+	
+}
+
+addLoadEvent(checkboxSelectAll)
